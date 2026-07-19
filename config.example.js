98943e889d512d8f3d8dae151dae8cbbf9711ad5
V <viraitrisha2006@gmail.com>
2026-07-19 14:49:13 -0300

    config.example.js added

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    apiKey: "YOUR_API_KEY_HERE",
+    apiKey: "API_KEY",
     geoUrl: "https://api.openweathermap.org/geo/1.0/direct",
     weatherUrl: "https://api.openweathermap.org/data/2.5/weather"
 };
